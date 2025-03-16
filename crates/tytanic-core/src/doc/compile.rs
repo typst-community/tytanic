2c311d1c5b0cc29bc6a08ab25ad86537def953ed
tinger <tinger@tinger.dev>
2025-03-16 13:33:50 +0100

    core: Refactor & expose `TestWorldAdapter`
    While the `root_prefix` solution on the adapter is not ideal it works and isn't
    a hack either.
    
    Closes #157.

--- crates/tytanic-core/src/doc/compile.rs
+++ crates/tytanic-core/src/doc/compile.rs
@@ -1,6 +1,7 @@
 //! Test document compilation and diagnostics handling.
 
 use std::fmt::Debug;
+use std::path::PathBuf;
 use std::sync::LazyLock;
 use std::sync::OnceLock;
 
@@ -10,6 +11,7 @@ use typst::diag::{FileResult, Severity, SourceDiagnostic, Warned};
 use typst::foundations::{Bytes, Datetime};
 use typst::layout::PagedDocument;
 use typst::syntax::package::PackageSpec;
+use typst::syntax::VirtualPath;
 use typst::syntax::{FileId, Source, Span};
 use typst::text::{Font, FontBook};
 use typst::utils::LazyHash;
@@ -20,6 +22,137 @@ use crate::library::augmented_default_library;
 
 static AUGMENTED_LIBRARY: LazyLock<LazyHash<Library>> =
     LazyLock::new(|| LazyHash::new(augmented_default_library()));
+
+/// A wrapper type around World implementations for compiling tests.
+///
+/// This type is exposed only within [`compile`].
+#[derive(Clone)]
+pub struct TestWorldAdapter<'w> {
+    base: &'w dyn World,
+    source: Source,
+    root_prefix: Option<PathBuf>,
+    augment: bool,
+    package: Option<PackageSpec>,
+    accessed_old: OnceLock<(PackageSpec, PackageSpec)>,
+}
+
+impl TestWorldAdapter<'_> {
+    /// Whether the standard library should be augmented.
+    ///
+    /// This can be used to allow unit tests to test error paths and inspect
+    /// panics. This can be omitted if the base world implementation already has
+    /// an augmented standard library, see [`augmented_library`][lib].
+    ///
+    /// [lib]: crate::library::augmented_library
+    pub fn augment_standard_library(&mut self, value: bool) -> &mut Self {
+        self.augment = value;
+        self
+    }
+
+    /// Add a root prefix to each [`FileId`].
+    ///
+    /// This can be used to allow template tests to access the correct files
+    /// when using absolute paths. A template test located in `$root/template`
+    /// would have absolute file id like `/refs.bib` resolve to
+    /// `/template/refs.bib` after which the base [`World`] implementation takes
+    /// with resolving the path over.
+    pub fn root_prefix(&mut self, value: Option<PathBuf>) -> &mut Self {
+        self.root_prefix = value;
+        self
+    }
+
+    /// Set the given package spec to be re-routed to the current project root.
+    ///
+    /// This can be used to allow template tests to import unreleased versions
+    /// of the currently tested package. Any [`FileId`] with this package spec
+    /// will stripped of it, accessing the project files instead.
+    pub fn reroute_package(&mut self, value: Option<PackageSpec>) -> &mut Self {
+        self.package = value;
+        self
+    }
+}
+
+impl TestWorldAdapter<'_> {
+    fn transform_id(&self, id: FileId) -> FileId {
+        let Some(this) = self.package.as_ref() else {
+            return id;
+        };
+
+        match id.package() {
+            Some(package) if package == this => FileId::new(None, id.vpath().clone()),
+            Some(package) => {
+                if package.namespace == this.namespace
+                    && package.name == this.name
+                    && package.version < this.version
+                {
+                    _ = self.accessed_old.set((package.clone(), this.clone()));
+                }
+
+                id
+            }
+            None => match self.root_prefix.as_ref() {
+                Some(prefix) => FileId::new(
+                    None,
+                    VirtualPath::new(prefix.join(id.vpath().as_rootless_path())),
+                ),
+                None => id,
+            },
+        }
+    }
+}
+
+impl World for TestWorldAdapter<'_> {
+    fn library(&self) -> &LazyHash<Library> {
+        if self.augment {
+            &AUGMENTED_LIBRARY
+        } else {
+            self.base.library()
+        }
+    }
+
+    fn book(&self) -> &LazyHash<FontBook> {
+        self.base.book()
+    }
+
+    fn main(&self) -> FileId {
+        match self.root_prefix.as_ref() {
+            Some(prefix) => {
+                let id = self.source.id();
+                let vpath = id.vpath().as_rootless_path();
+
+                FileId::new(
+                    id.package().cloned(),
+                    VirtualPath::new(vpath.strip_prefix(prefix).unwrap_or(vpath)),
+                )
+            }
+            None => self.source.id(),
+        }
+    }
+
+    fn source(&self, id: FileId) -> FileResult<Source> {
+        let id = self.transform_id(id);
+
+        if id == self.source.id() {
+            Ok(self.source.clone())
+        } else {
+            self.base.source(id)
+        }
+    }
+
+    fn file(&self, id: FileId) -> FileResult<Bytes> {
+        let id = self.transform_id(id);
+
+        self.base.file(id)
+    }
+
+    fn font(&self, index: usize) -> Option<Font> {
+        self.base.font(index)
+    }
+
+    fn today(&self, offset: Option<i64>) -> Option<Datetime> {
+        self.base.today(offset)
+    }
+}
 
 /// How to handle warnings during compilation.
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
@@ -45,98 +178,30 @@ pub struct Error(pub EcoVec<SourceDiagnostic>);
 ///
 /// This function compiles a test source by wrapping the provided [`World`]
 /// implementation in a short lived wrapper which exposes `source` as the
-/// compilation entrypoint. An optional package spec can be provided for
-/// re-routing package imports to the local root, this is primarily useful for
-/// template tests which access unreleased versions of a package.
-pub fn compile(
+/// compilation entrypoint. The `f` argument can be used to configure the
+/// behavior of this wrapper type.
+pub fn compile<'w, F>(
     source: Source,
-    world: &dyn World,
-    augment: bool,
-    reroute_to_self: Option<&PackageSpec>,
+    world: &'w dyn World,
     warnings: Warnings,
-) -> Warned<Result<PagedDocument, Error>> {
-    struct TestWorldAdapter<'s, 'w, 'p> {
-        source: &'s Source,
-        global: &'w dyn World,
-        augment: bool,
-        package: Option<&'p PackageSpec>,
-        accessed_old: OnceLock<(PackageSpec, PackageSpec)>,
-    }
-
-    impl TestWorldAdapter<'_, '_, '_> {
-        fn transform_id(&self, id: FileId) -> FileId {
-            let Some(this) = self.package else {
-                return id;
-            };
-
-            match id.package() {
-                Some(pacakge) if pacakge == this => FileId::new(None, id.vpath().clone()),
-                Some(package) => {
-                    if package.namespace == this.namespace && package.name == this.name {
-                        _ = self.accessed_old.set((package.clone(), this.clone()));
-                    }
-
-                    id
-                }
-                None => id,
-            }
-        }
-    }
-
-    impl World for TestWorldAdapter<'_, '_, '_> {
-        fn library(&self) -> &LazyHash<Library> {
-            if self.augment {
-                &AUGMENTED_LIBRARY
-            } else {
-                self.global.library()
-            }
-        }
-
-        fn book(&self) -> &LazyHash<FontBook> {
-            self.global.book()
-        }
-
-        fn main(&self) -> FileId {
-            self.source.id()
-        }
-
-        fn source(&self, id: FileId) -> FileResult<Source> {
-            let id = self.transform_id(id);
-
-            if id == self.source.id() {
-                Ok(self.source.clone())
-            } else {
-                self.global.source(id)
-            }
-        }
-
-        fn file(&self, id: FileId) -> FileResult<Bytes> {
-            let id = self.transform_id(id);
-
-            self.global.file(id)
-        }
-
-        fn font(&self, index: usize) -> Option<Font> {
-            self.global.font(index)
-        }
-
-        fn today(&self, offset: Option<i64>) -> Option<Datetime> {
-            self.global.today(offset)
-        }
-    }
-
-    let test_world = TestWorldAdapter {
-        source: &source,
-        global: world,
-        augment,
-        package: reroute_to_self,
+    f: F,
+) -> Warned<Result<PagedDocument, Error>>
+where
+    F: for<'a> FnOnce(&'a mut TestWorldAdapter<'w>) -> &'a mut TestWorldAdapter<'w>,
+{
+    let mut test_world = TestWorldAdapter {
+        base: world,
+        source,
+        root_prefix: None,
+        augment: false,
+        package: None,
         accessed_old: OnceLock::new(),
     };
 
     let Warned {
         output,
         warnings: mut emitted,
-    } = typst::compile(&test_world);
+    } = typst::compile(f(&mut test_world));
 
     if let Some((old, new)) = test_world.accessed_old.into_inner() {
         emitted.push(SourceDiagnostic {
@@ -203,7 +268,7 @@ mod tests {
         let world = VirtualWorld::default();
         let source = Source::detached(TEST_PASS);
 
-        let Warned { output, warnings } = compile(source, &world, false, None, Warnings::Ignore);
+        let Warned { output, warnings } = compile(source, &world, Warnings::Ignore, |w| w);
         assert!(output.is_ok());
         assert!(warnings.is_empty());
     }
@@ -213,7 +278,7 @@ mod tests {
         let world = VirtualWorld::default();
         let source = Source::detached(TEST_PASS);
 
-        let Warned { output, warnings } = compile(source, &world, false, None, Warnings::Emit);
+        let Warned { output, warnings } = compile(source, &world, Warnings::Emit, |w| w);
         assert!(output.is_ok());
         assert!(warnings.is_empty());
     }
@@ -223,7 +288,7 @@ mod tests {
         let world = VirtualWorld::default();
         let source = Source::detached(TEST_PASS);
 
-        let Warned { output, warnings } = compile(source, &world, false, None, Warnings::Promote);
+        let Warned { output, warnings } = compile(source, &world, Warnings::Promote, |w| w);
         assert!(output.is_ok());
         assert!(warnings.is_empty());
     }
@@ -233,7 +298,7 @@ mod tests {
         let world = VirtualWorld::default();
         let source = Source::detached(TEST_WARN);
 
-        let Warned { output, warnings } = compile(source, &world, false, None, Warnings::Ignore);
+        let Warned { output, warnings } = compile(source, &world, Warnings::Ignore, |w| w);
         assert!(output.is_ok());
         assert!(warnings.is_empty());
     }
@@ -243,7 +308,7 @@ mod tests {
         let world = VirtualWorld::default();
         let source = Source::detached(TEST_WARN);
 
-        let Warned { output, warnings } = compile(source, &world, false, None, Warnings::Emit);
+        let Warned { output, warnings } = compile(source, &world, Warnings::Emit, |w| w);
         assert!(output.is_ok());
         assert_eq!(warnings.len(), 1);
     }
@@ -253,7 +318,7 @@ mod tests {
         let world = VirtualWorld::default();
         let source = Source::detached(TEST_WARN);
 
-        let Warned { output, warnings } = compile(source, &world, false, None, Warnings::Promote);
+        let Warned { output, warnings } = compile(source, &world, Warnings::Promote, |w| w);
         assert_eq!(output.unwrap_err().0.len(), 1);
         assert!(warnings.is_empty());
     }
@@ -263,7 +328,7 @@ mod tests {
         let world = VirtualWorld::default();
         let source = Source::detached(TEST_FAIL);
 
-        let Warned { output, warnings } = compile(source, &world, false, None, Warnings::Ignore);
+        let Warned { output, warnings } = compile(source, &world, Warnings::Ignore, |w| w);
         assert_eq!(output.unwrap_err().0.len(), 1);
         assert!(warnings.is_empty());
     }
@@ -273,7 +338,7 @@ mod tests {
         let world = VirtualWorld::default();
         let source = Source::detached(TEST_FAIL);
 
-        let Warned { output, warnings } = compile(source, &world, false, None, Warnings::Emit);
+        let Warned { output, warnings } = compile(source, &world, Warnings::Emit, |w| w);
         assert_eq!(output.unwrap_err().0.len(), 1);
         assert_eq!(warnings.len(), 1);
     }
@@ -283,7 +348,7 @@ mod tests {
         let world = VirtualWorld::default();
         let source = Source::detached(TEST_FAIL);
 
-        let Warned { output, warnings } = compile(source, &world, false, None, Warnings::Promote);
+        let Warned { output, warnings } = compile(source, &world, Warnings::Promote, |w| w);
         assert_eq!(output.unwrap_err().0.len(), 2);
         assert!(warnings.is_empty());
     }
