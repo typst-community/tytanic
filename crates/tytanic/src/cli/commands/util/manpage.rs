7e4776379db71536d775bac5584519acec1437b9
tinger <tinger@tinger.dev>
2025-05-02 18:42:07 +0200

    cli: Add manpage generation

---
+++ crates/tytanic/src/cli/commands/util/manpage.rs
@@ -0,0 +1,23 @@
+use std::path::PathBuf;
+
+use clap::CommandFactory;
+use color_eyre::eyre;
+
+use crate::cli::Context;
+
+#[derive(clap::Args, Debug, Clone)]
+#[group(id = "util-manpage-args")]
+pub struct Args {
+    /// The directory to write the man pages to
+    #[arg(default_value = ".")]
+    pub dir: PathBuf,
+}
+
+pub fn run(_ctx: &mut Context, args: &Args) -> eyre::Result<()> {
+    let mut cmd = crate::CliArguments::command();
+    cmd.set_bin_name(std::env!("CARGO_BIN_NAME"));
+
+    clap_mangen::generate_to(cmd, &args.dir)?;
+
+    Ok(())
+}
