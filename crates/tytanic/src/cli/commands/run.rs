ef8969480e67a168283fb97a3056806391fad8e4
tinger <tinger@tinger.dev>
2025-05-02 19:44:10 +0200

    runner: Extract common action options to config

--- crates/tytanic/src/cli/commands/run.rs
+++ crates/tytanic/src/cli/commands/run.rs
@@ -65,18 +65,17 @@ pub fn run(ctx: &mut Context, args: &Args) -> eyre::Result<()> {
             optimize: args.export.optimize_refs.get_or_default(),
             fail_fast: args.runner.fail_fast.get_or_default(),
             pixel_per_pt,
-            action: Action::Run {
-                strategy: args
-                    .compare
-                    .compare
-                    .get_or_default()
-                    .then_some(Strategy::Simple {
-                        max_delta,
-                        max_deviation,
-                    }),
-                export_ephemeral: args.export.export_ephemeral.get_or_default(),
-                origin,
-            },
+            strategy: args
+                .compare
+                .compare
+                .get_or_default()
+                .then_some(Strategy::Simple {
+                    max_delta,
+                    max_deviation,
+                }),
+            export_ephemeral: args.export.export_ephemeral.get_or_default(),
+            origin,
+            action: Action::Run,
             cancellation: &CANCELLED,
         },
     );
