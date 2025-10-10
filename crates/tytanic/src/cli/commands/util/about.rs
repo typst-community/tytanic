28c598f042acc58f481d12f3a1c90f9ba6d7d2b9
tinger <tinger@tinger.dev>
2025-10-10 23:44:50 +0200

    cli: Ensure `cargo publish` works
    This comes with the downside of once again having to do the update in
    the about page manually.

--- crates/tytanic/src/cli/commands/util/about.rs
+++ crates/tytanic/src/cli/commands/util/about.rs
@@ -7,7 +7,7 @@ use super::Context;
 pub fn run(ctx: &mut Context) -> eyre::Result<()> {
     let mut w = ctx.ui.stderr();
     writeln!(w, "Version: {}", env!("CARGO_PKG_VERSION"))?;
-    writeln!(w, "Typst Version: {}", env!("TYTANIC_TYPST_VERSION"))?;
+    writeln!(w, "Typst Version: 0.14.0-rc.1")?;
 
     Ok(())
 }
