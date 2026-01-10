45e2d979c8e77ef6358722803b656f6cf69bcc72
tinger <tinger@tinger.dev>
2026-01-10 16:37:21 +0100

    flake: Switch formatter to nixfmt-tree

--- flake.nix
+++ flake.nix
@@ -18,111 +18,137 @@
     };
   };
 
-  outputs = inputs@{ flake-parts, crane, nixpkgs, fenix, rust-manifest, self, ... }: flake-parts.lib.mkFlake { inherit inputs; } {
-    systems = import inputs.systems;
+  outputs =
+    inputs@{
+      flake-parts,
+      crane,
+      nixpkgs,
+      fenix,
+      rust-manifest,
+      self,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
 
-    imports = [
-      inputs.flake-parts.flakeModules.easyOverlay
-    ];
+      imports = [
+        inputs.flake-parts.flakeModules.easyOverlay
+      ];
 
-    perSystem = { self', pkgs, lib, system, ... }:
-      let
-        cargoToml = lib.importTOML ./Cargo.toml;
+      perSystem =
+        {
+          self',
+          pkgs,
+          lib,
+          system,
+          ...
+        }:
+        let
+          cargoToml = lib.importTOML ./Cargo.toml;
 
-        pname = "tytanic";
-        version = cargoToml.workspace.package.version;
+          pname = "tytanic";
+          version = cargoToml.workspace.package.version;
 
-        rust-toolchain = (fenix.packages.${system}.fromManifestFile rust-manifest).defaultToolchain;
+          rust-toolchain = (fenix.packages.${system}.fromManifestFile rust-manifest).defaultToolchain;
 
-        # Crane-based Nix flake configuration.
-        # Based on https://github.com/ipetkov/crane/blob/master/examples/trunk-workspace/flake.nix
-        craneLib = (crane.mkLib pkgs).overrideToolchain rust-toolchain;
+          # Crane-based Nix flake configuration.
+          # Based on https://github.com/ipetkov/crane/blob/master/examples/trunk-workspace/flake.nix
+          craneLib = (crane.mkLib pkgs).overrideToolchain rust-toolchain;
 
-        # Tytanic files to include in the derivation.
-        # Here we include Rust files.
-        src = lib.fileset.toSource {
-          root = ./.;
-          fileset = lib.fileset.unions [
-            ./Cargo.toml
-            ./Cargo.lock
-            ./rustfmt.toml
-            ./crates
-            ./assets
-          ];
+          # Tytanic files to include in the derivation.
+          # Here we include Rust files.
+          src = lib.fileset.toSource {
+            root = ./.;
+            fileset = lib.fileset.unions [
+              ./Cargo.toml
+              ./Cargo.lock
+              ./rustfmt.toml
+              ./crates
+              ./assets
+            ];
+          };
+
+          # Tytanic derivation's args, used within crane's derivation
+          # generation functions.
+          commonCraneArgs = {
+            inherit src pname version;
+
+            buildInputs = [
+              pkgs.openssl
+            ];
+
+            nativeBuildInputs = [
+              pkgs.pkg-config
+              pkgs.openssl.dev
+            ];
+          };
+
+          # Derivation with just the dependencies, so we don't have to keep
+          # re-building them.
+          cargoArtifacts = craneLib.buildDepsOnly commonCraneArgs;
+
+          tytanic = craneLib.buildPackage (
+            commonCraneArgs
+            // {
+              inherit cargoArtifacts;
+
+              nativeBuildInputs = commonCraneArgs.nativeBuildInputs ++ [
+                pkgs.installShellFiles
+              ];
+
+              GEN_ARTIFACTS = "artifacts";
+              TYTANIC_VERSION =
+                let
+                  rev = self.shortRev or "dirty";
+                  version = cargoToml.workspace.package.version;
+                in
+                "${version} (${rev})";
+
+              meta.mainProgram = "tt";
+            }
+          );
+        in
+        {
+          formatter = pkgs.nixfmt-tree;
+
+          packages = {
+            default = tytanic;
+            tytanic-dev = self'.packages.default;
+          };
+
+          overlayAttrs = builtins.removeAttrs self'.packages [ "default" ];
+
+          apps.default = {
+            type = "app";
+            program = lib.getExe tytanic;
+          };
+
+          checks = {
+            tytanic-fmt = craneLib.cargoFmt commonCraneArgs;
+            tytanic-clippy = craneLib.cargoClippy (
+              commonCraneArgs
+              // {
+                inherit cargoArtifacts;
+                cargoClippyExtraArgs = "--workspace -- --deny warnings";
+              }
+            );
+            tytanic-test = craneLib.cargoTest (
+              commonCraneArgs
+              // {
+                inherit cargoArtifacts;
+                cargoTestExtraArgs = "--workspace";
+              }
+            );
+          };
+
+          devShells.default = craneLib.devShell {
+            checks = self'.checks;
+            inputsFrom = [ tytanic ];
+
+            buildInputs = with pkgs; [
+              rust-analyzer
+            ];
+          };
         };
-
-        # Tytanic derivation's args, used within crane's derivation
-        # generation functions.
-        commonCraneArgs = {
-          inherit src pname version;
-
-          buildInputs = [
-            pkgs.openssl
-          ];
-
-          nativeBuildInputs = [
-            pkgs.pkg-config
-            pkgs.openssl.dev
-          ];
-        };
-
-        # Derivation with just the dependencies, so we don't have to keep
-        # re-building them.
-        cargoArtifacts = craneLib.buildDepsOnly commonCraneArgs;
-
-        tytanic = craneLib.buildPackage (commonCraneArgs // {
-          inherit cargoArtifacts;
-
-          nativeBuildInputs = commonCraneArgs.nativeBuildInputs ++ [
-            pkgs.installShellFiles
-          ];
-
-          GEN_ARTIFACTS = "artifacts";
-          TYTANIC_VERSION =
-            let
-              rev = self.shortRev or "dirty";
-              version = cargoToml.workspace.package.version;
-            in
-            "${version} (${rev})";
-
-          meta.mainProgram = "tt";
-        });
-      in
-      {
-        formatter = pkgs.nixpkgs-fmt;
-
-        packages = {
-          default = tytanic;
-          tytanic-dev = self'.packages.default;
-        };
-
-        overlayAttrs = builtins.removeAttrs self'.packages [ "default" ];
-
-        apps.default = {
-          type = "app";
-          program = lib.getExe tytanic;
-        };
-
-        checks = {
-          tytanic-fmt = craneLib.cargoFmt commonCraneArgs;
-          tytanic-clippy = craneLib.cargoClippy (commonCraneArgs // {
-            inherit cargoArtifacts;
-            cargoClippyExtraArgs = "--workspace -- --deny warnings";
-          });
-          tytanic-test = craneLib.cargoTest (commonCraneArgs // {
-            inherit cargoArtifacts;
-            cargoTestExtraArgs = "--workspace";
-          });
-        };
-
-        devShells.default = craneLib.devShell {
-          checks = self'.checks;
-          inputsFrom = [ tytanic ];
-
-          buildInputs = with pkgs; [
-            rust-analyzer
-          ];
-        };
-      };
-  };
+    };
 }
